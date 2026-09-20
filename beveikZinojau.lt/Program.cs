0d8c64082cf86b2a1e8a6a891835cdd70cbba880
columbidae-code <ugnuze.mo@gmail.com>
2026-09-21 02:09:35 +0300

    Added hardcoded login and registration backend

--- beveikZinojau.lt/Program.cs
+++ beveikZinojau.lt/Program.cs
@@ -1,4 +1,5 @@
 var builder = WebApplication.CreateBuilder(args);
+builder.WebHost.UseUrls("http://localhost:5159");
 
 // Container services
 builder.Services.AddControllers();
@@ -8,7 +9,7 @@ builder.Services.AddCors(options =>
 {
     options.AddPolicy("AllowReactApp", policy =>
     {
-        policy.WithOrigins("http://localhost:5173") // Vite default port
+        policy.WithOrigins("http://localhost:5159") // Vite default port
               .AllowAnyHeader()
               .AllowAnyMethod();
     });
